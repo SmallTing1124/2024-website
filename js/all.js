c6b08e3bc9ba3282e2e82ebb89460aa3e2cd4ebc
SmallTing1124 <chentt1124@gmail.com>
2024-05-11 01:12:16 +0800

    新增service.html

---
+++ js/all.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".articleSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
